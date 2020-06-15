6b5dc678a384f58a1f2d4e6e6a8aa61162aa236d
Jack Collins <jackpaulcollins@gmail.com>
2020-06-15 16:38:59 -0700

    add new relic custom event

--- src/utils/geocode.js
+++ src/utils/geocode.js
@@ -1,6 +1,23 @@
 const request = require('request')
+const fetch = require('node-fetch')
 
 const geocode = (address, callback) => {
+  //sends custom event to New Relic
+  const insightsBody = {
+    eventType: 'addressCheck',
+    address
+  }
+  fetch('https://insights-collector.newrelic.com/v1/accounts/2478897/events', {
+    method: 'post',
+    body:    JSON.stringify(insightsBody),
+    headers: { 'X-Insert-Key': 'NRII-_16lcfUCNXyYhrSuOBCkLabQ5yukUg4u',
+               'Content-Type': 'application/json',
+               'Content-Encoding' : 'gzip'
+               },
+    
+}).then(res => res.json())
+  .then(json => console.log(json));
+
   const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodeURIComponent(address) + ' .json?access_token=pk.eyJ1IjoiamFja3BhdWxjb2xsaW5zIiwiYSI6ImNrOTY0dWU2dDBoZWczZnAyOTNlN25ueTAifQ.q6OMrlkluFoMAsCipYiX3w&limit=1'
 
   request({ url, json: true }, (error, { body }) => {
