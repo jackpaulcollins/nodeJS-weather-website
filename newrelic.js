fdf27162f499cd7e51e10bad04c0a208652a929c
Jack Collins <jackpaulcollins@gmail.com>
2020-10-29 09:44:52 -0700

    change app name

--- newrelic.js
+++ newrelic.js
@@ -9,7 +9,7 @@ exports.config = {
   /**
    * Array of application names.
    */
-  app_name: ['WeatherApp_testing_bug'],
+  app_name: ['WeatherApp_testing_bug_restarted'],
   /**
    * Your New Relic license key.
    */
